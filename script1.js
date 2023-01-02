617dc5bbbaebc7831fbda3b214bf2afac3ae1e1c
kowsyjacklin <120628310+kowsyjacklin@users.noreply.github.com>
2023-01-02 11:12:05 +0530

    javascript

---
+++ script1.js
@@ -0,0 +1,49 @@
+var head1=[ 
+    {
+        
+           careerobjective : "To be a part of company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+    },
+    {
+    summary : "strong understand of technical information and devision forward thinking effective solutions"
+    },
+    {
+        personalskills:"interpersonal skills, critical thinking, problem solving."
+    }
+    ]
+     var second =[
+    {
+        Qualification:'BE-ECE',
+        college:'university college of engineering,kanchipuram',
+        yearofpassing:'2022',
+        percentageCGPA:'84%'
+    },
+    {
+        Qualification:'Diploma',
+        college:'central polytechnic chennai',
+        yearofpassing:'2019',
+        percentageCGPA:'80%'
+    },
+    {
+        Qualification:'SSLC',
+        School:'Sri Rajareshwari matriculation school',
+        Board:'State board',
+        yearofpassing:'2016',
+        percentageCGPA:'84%'
+    }
+     ]
+    
+     var skills=
+        ["c", "python" ,"HTML5", "CSS3", "JAVASCRIPT", "ms office"]
+        var personalinfo={
+    name:"Kowsalya k",
+    DOB:"15-05-2001",
+    Gender:"Female",
+    Maritalstatus:"single",
+    Nationality:"Indian",
+    Religion:"Hindu",
+    Mobileno:6384015212
+        }
+    console.log(head1);
+    console.table(second)
+    console.log(skills)
+    console.table(personalinfo)
